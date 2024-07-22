077f35d148fd53aaf28f8fb9703cde4147b6ab69
satish-nirmal <satishnirmal16@gmail.com>
2024-07-22 23:00:36 +0530

    added pattern 3 program

---
+++ Pattern3.java
@@ -0,0 +1,18 @@
+package com.starpattern.numbers;
+
+public class Pattern3 {
+	public static void main(String[] args) {
+
+		// Outer loop for number of rows
+		for (int i = 1; i <= 5; i++) {
+
+			// Inner loop for number of stars in each row
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+
+			// Move to the next line after printing stars in each row
+			System.out.println();
+		}
+	}
+}
