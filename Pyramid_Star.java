7bc04b4a154aa636bac0334510700562b8cb229a
satish-nirmal <satishnirmal16@gmail.com>
2024-07-24 19:35:19 +0530

    added pyramid program

---
+++ Pyramid_Star.java
@@ -0,0 +1,20 @@
+package com.starpattern.numbers;
+
+public class Pyramid_Star {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 5; i++) {
+			// Print spaces
+			
+			for (int j = 5; j > i; j--) {
+				System.out.print(" ");
+			}
+			
+			// Print asterisks
+			for (int k = 0; k <= i; k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
