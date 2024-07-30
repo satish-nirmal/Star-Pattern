d908bec6bc56c93389f1c466aa5309cb4fa19ee6
satish-nirmal <satishnirmal16@gmail.com>
2024-07-30 22:00:29 +0530

    inserted the comments in program

--- Pattern2.java
+++ Pattern2.java
@@ -13,9 +13,11 @@ public class Pattern2 {
 
 	public static void main(String[] args) {
 
-		for (int i = 1; i <= 5; i++) {
-
-			for (int j = 1; j <= i; j++) {
+        // Outer loop for number of rows
+		for(int i = 1 ; i <= 5; i++) {
+			
+		// Inner loop for number of stars in each row
+			for (int j = 1 ; j <= i ; j++) {
 
 				System.out.print("* ");
 			}
