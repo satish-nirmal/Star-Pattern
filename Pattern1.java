c07a8d198f5ea08e2213e339f845d6120478a515
satish-nirmal <satishnirmal16@gmail.com>
2024-08-01 22:17:23 +0530

    add commented lines on the program

--- Pattern1.java
+++ Pattern1.java
@@ -13,8 +13,10 @@ public class Pattern1 {
 
 	public static void main(String[] args) {
 
+		// Outer loop for number of rows
 		for (int i = 1; i <= 5; i++) {
 
+		// Inner loop for number of stars in each row
 			for (int j = 1; j <= 5; j++) {
 				System.out.print("* ");
 
