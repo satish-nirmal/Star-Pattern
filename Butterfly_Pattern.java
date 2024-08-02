ca168cf9e11693cbb3575e337e5287d9672686d0
satish-nirmal <satishnirmal16@gmail.com>
2024-08-02 23:27:47 +0530

    add butterfly program

---
+++ Butterfly_Pattern.java
@@ -0,0 +1,42 @@
+package com.starpattern.numbers;
+
+public class Butterfly_Pattern {
+
+	public static void main(String[] args) {
+		int n = 5;
+
+		// Upper half of the diamond
+		for (int i = 1; i <= n; i++) {
+			// Print left half of the row
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			// Print spaces in the middle
+			for (int j = 1; j <= 2 * (n - i); j++) {
+				System.out.print(" ");
+			}
+			// Print right half of the row
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		// Lower half of the diamond
+//		for (int i = n; i >= 1; i--) {
+//			// Print left half of the row
+//			for (int j = 1; j <= i; j++) {
+//				System.out.print("*");
+//			}
+//			// Print spaces in the middle
+//			for (int j = 1; j <= 2 * (n - i); j++) {
+//				System.out.print(" ");
+//			}
+//			// Print right half of the row
+//			for (int j = 1; j <= i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+	}
+}
