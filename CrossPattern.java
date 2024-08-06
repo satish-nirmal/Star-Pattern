83bec84dd5d27efd2658c2ee7d63b51ba2fd5e6c
satish-nirmal <satishnirmal16@gmail.com>
2024-08-06 21:38:55 +0530

    added cross start pattern program

---
+++ CrossPattern.java
@@ -0,0 +1,22 @@
+package com.starpattern.numbers;
+
+public class CrossPattern {
+
+	public static void main(String[] args) {
+		int n = 7; // Must be odd
+
+		for (int i = 0; i < n; i++) {
+
+			for (int j = 0; j < n; j++) {
+				// Check if the current position is in the center row or center column
+				if (i == n / 2 || j == n / 2) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			// Move to the next line after each row
+			System.out.println();
+		}
+	}
+}
