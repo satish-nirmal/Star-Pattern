31b770b687a072f8ffdc39137c49b3ec8b8b1280
satish-nirmal <satishnirmal16@gmail.com>
2024-08-12 22:05:23 +0530

    right up program

---
+++ Program4_Right_up.java
@@ -0,0 +1,22 @@
+package com.starpattern.numbers;
+
+public class Program4_Right_up {
+	
+	public static void main(String[] args) {
+
+		// size of the triangle
+		int size = 5;
+		// loop to print the pattern
+		for (int i = 0; i < size; i++) {
+			// print spaces
+			for (int j = 1; j < size - i; j++) {
+				System.out.print(" ");
+			}
+			// print stars
+			for (int k = 0; k <= i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
