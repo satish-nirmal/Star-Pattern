8ba9131936c0e41d7b9e8973f6620e153a14a412
satish-nirmal <satishnirmal16@gmail.com>
2024-08-17 13:31:32 +0530

    added squarePattern program

---
+++ Program0_Squre.java
@@ -0,0 +1,18 @@
+package com.starpattern.numbers;
+
+public class Program0_Squre {
+
+	public static void main(String[] args) {
+
+		// size of the square
+		int size = 5;
+		// outer loop
+		for (int i = 0; i < size; i++) {
+			// inner loop
+			for (int j = 0; j < size; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
